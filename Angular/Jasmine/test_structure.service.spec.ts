747dfb0b531937b528ab82692f061ec9989f8d3c
DiegoSkieresz <46655332+SkiereszDiego@users.noreply.github.com>
2022-11-07 16:03:14 -0300

    Update test_structure.service.spec.ts

--- Angular/Jasmine/test_structure.service.spec.ts
+++ Angular/Jasmine/test_structure.service.spec.ts
@@ -30,6 +30,18 @@ describe(UniqueIDService.name, () => {
   it(`#${UniqueIDService.prototype.generateUniqueidWithPrefix.name} SHOULD generate id WHEN called with prefix`, () => {
     const service = new UniqueIdService()
     const id = service.generateUniqueIdWithPrefix('app');
-    expect(id.startsWith('app-')).tobeTrue();
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+});
+
+<!-- Testar para nao repetir id -->
+describe(UniqueIDService.name, () => {
+  it(`#${UniqueIDService.prototype.generateUniqueidWithPrefix.name} SHOULD generate id WHEN called with prefix`, () => {
+    const service = new UniqueIdService()
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
   });
 });
