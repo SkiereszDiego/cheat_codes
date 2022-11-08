74a934564878f04b0de1394fd5043755db8ac74b
DiegoSkieresz <46655332+SkiereszDiego@users.noreply.github.com>
2022-11-07 22:13:55 -0300

    Update exception.service.sepc.ts

--- Angular/Jasmine/exception.service.sepc.ts
+++ Angular/Jasmine/exception.service.sepc.ts
@@ -1,14 +1,26 @@
-<!-- 
-Karma roda os testes de maneira aleatoria, ele faz isso para pegar o erro de um teste vazar e pegar o contexto de outro teste.
-Garantir com o beforeEach que tenha uma instancia unica para cada it, para que ela exista so durante o teste
 
--->
+// Karma roda os testes de maneira aleatoria, ele faz isso para pegar o erro de um teste vazar e pegar o contexto de outro teste.
+// Garantir com o beforeEach que tenha uma instancia unica para cada it, para que ela exista so durante o teste
+
 import { UniqueIDService } from '.unique-id.service';
 
 describe(UniqueIDService.name, () => {
   it(`#${UniqueIDService.prototype.generateUniqueidWithPrefix.name}
     should generate id when called with prefix`, () => {
-    toTrow- se ela lança algo queremos capturar e saber qual problema
-    expect(service.generateUniqueIdWithPrefix(null)).toThrow(); 
-  });
+//     toTrow- se ela lança algo queremos capturar e saber qual problema
+    
+//     expect(service.generateUniqueIdWithPrefix(null)).toThrow();
+//     ---- Vai falhar pq o jasmine n consegue entender que a exceção era pra ser sucesso pq era esperada
+//     ---- Se vc esta testando um metodo tu tem que char ele dentro de uma funcao
+    
+//     expect(() => service.generateUniqueIdWithPrefix(null)).toThrow();
+//     expect(() => service.generateUniqueIdWithPrefix(undefined)).toThrow();
+//     expect(() => service.generateUniqueIdWithPrefix('')).toThrow();
+    
+//     ----deizando organizado
+      const emptyValues = [null, undefined, ''];
+      emptyValues.forEach(emptyValues => {
+       expect(() => service.generateUniqueIdWithPrefix(emptyValues)).toThrow();
+      });
+    });
 });
